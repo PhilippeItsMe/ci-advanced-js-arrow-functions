be7caee3445efd807cc4eb3440d62d01fc02129c
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2024-09-17 07:42:28 +0000

    Arrow function, advanced JS

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,36 @@
 
 // Vanilla JavaScript Function
 
+function addTwoNumbers(a,b) {
+    return a + b;
+}
+let sum = addTwoNumbers (3,5);
+console.log(sum);
+
 // Arrow Function With Parameters
+
+const addTwoNumbers = (a,b) => {
+    return a + b;
+}
+let sum = addTwoNumbers (3,5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
 
-// Implicit Returns
+const addTwoNumbers = (a,b) => a + b;
+const addTwoNumbers = (a,b) => (a + b); //also valid
+
+ // Implicit Returns
+
+const saySomething = message => console.log(message);
+saySomething ('Hello there!!');
 
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `
+    <p> Hello World</p>
+    `
+)
+
+console.log (returnMultipleLines());
